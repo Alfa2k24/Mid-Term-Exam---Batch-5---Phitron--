a874cdcb21ae1b4cfa510bd139b9511e7a5bd964
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-04-26 09:30:00 +0600

    Solution Accepted

--- D_number_problem.c
+++ D_number_problem.c
@@ -1,6 +1,34 @@
 #include<stdio.h>
-int main(){
+#include<string.h>
 
-    
+int main(){
+char ch[10000];
+int t;
+
+scanf("%d",&t);
+for(int i = 0; i < t; i++){
+    int countSmallLatter = 0, countNumber = 0, countCapitalLatter = 0;
+
+scanf("%s",ch);
+int len = strlen(ch);
+
+for(int j = 0; j < len; j++){
+    if(ch[j] >= 'a' && ch[j]<= 'z')
+    {
+    countSmallLatter++;
+    }
+     else if(ch[j] >= '0' && ch[j] <= '9'){
+     countNumber++;
+     } 
+     else if(ch[j] >= 'A' && ch[j] <= 'Z'){
+        countCapitalLatter++;
+     }
+}
+     printf("%d %d %d\n",countCapitalLatter, countSmallLatter, countNumber);
+
+}
     return 0;
 }
+
+
+// Accepted 
--- G_number_problem.c
+++ G_number_problem.c
@@ -1,6 +1,44 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-    
+int main() {
+    int t;
+    scanf("%d",&t);
+
+    for(int i = 0; i < t; i++){
+
+    int n;
+    scanf("%d",&n);
+    long long int arr[n];   
+   
+  for(int j = 0; j < n; j++){
+         scanf("%lld", &arr[j]);}
+
+    int value;
+        scanf("%d",&value);
+
+int count = 0;
+
+         for(int k = 0; k < n; k++){
+    if(arr[k] == value){
+    count++;
+break;    
+}          
+}
+
+if(count != 0){
+printf("YES\n");
+} else{
+printf("NO\n");
+}
+
+
+}
+
+
     return 0;
 }
+
+// Accepted
