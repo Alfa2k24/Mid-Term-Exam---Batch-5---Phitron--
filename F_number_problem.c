936f7fb8a9b2d4bd2f51b52d6bc5597a61002633
Alfa2k24 <alfa.sunny.2k24@gmail.com>
2024-04-28 20:52:38 +0600

    Solution Accepted

--- F_number_problem.c
+++ F_number_problem.c
@@ -1,6 +1,21 @@
 #include<stdio.h>
+#include<stdio.h>
+
 int main(){
 
+    char ch;
+    int fq[26] = {0};
+    while(scanf("%c",&ch) !=EOF){
+        int index = ch - 'a'; // a = 97
+        fq[index] = fq[index] + 1;
+
+    }
+
+    for(int i = 0; i < 26; i++){
+        if(fq[i] > 0){
+            printf("%c - %d\n", i+97, fq[i]);
+        }
+    }
     
     return 0;
 }
